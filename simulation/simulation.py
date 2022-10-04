f785322e74b1eaf9e89a8022b236208246bd1e88
jricci1 <joaquin.ricci@thenotcompany.com>
2022-10-03 21:47:25 -0300

    Use results on simulation classes wip

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,18 +1,26 @@
 # from pyswmm import Simulation, Nodes, Subcatchments, Links
-from typing import List
-import pandas as pd
-from collections import OrderedDict
-from .results import SubcatchmentsTimeSeriesResult, StorageUnitsTimeSeriesResult, JuntionsTimeSeriesResult, LinksTimeSeriesResult
 from pyswmm import Simulation
-from flopy.modflow import Modflow
+
 from georef import CoupledModel
+
+from .results import (
+    JuntionsTimeSeriesResult,
+    LinksTimeSeriesResult,
+    StorageUnitsTimeSeriesResult,
+    SubcatchmentsTimeSeriesResult,
+)
 
 SWMM_path = ""
 
+
 class SimulationResults:
     def __init__(self, simulation) -> None:
-        self.subcatchments_time_series_result: SubcatchmentsTimeSeriesResult = SubcatchmentsTimeSeriesResult(simulation)
-        self.storage_units_time_series_result: List[pd.DataFrame] = []
+        self.subcatchments_time_series_result = SubcatchmentsTimeSeriesResult(
+            simulation
+        )
+        self.storage_units_time_series_result = StorageUnitsTimeSeriesResult(simulation)
+        self.junctions_time_series_result = JuntionsTimeSeriesResult(simulation)
+        self.conduits_time_series_result = LinksTimeSeriesResult(simulation)
 
 
 def get_modflow_step_data():
@@ -23,6 +31,7 @@ def get_modflow_step_data():
     """
     ...
 
+
 def get_pyswmm_step_data():
     """From every pyswmm step we want to retreive:
     - Calculate the infiltration volume of the step per subcatchment (t+1) - t
@@ -32,12 +41,14 @@ def get_pyswmm_step_data():
     """
     ...
 
+
 def make_something_with_the_data():
     """
     1. Calculate pyswmm data
-    2. Add infiltration as 
+    2. Add infiltration as
     """
     ...
+
 
 class SmartSimulation(Simulation):
     """An extension of pyswmm simulation to allow passing callbacks to be executed in a fixed period
@@ -46,18 +57,19 @@ class SmartSimulation(Simulation):
     Args:
         Simulation (_type_): PYSMM simulation class
     """
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._callbacks["add_on_x_seconds_before_step"] = None
         self._callbacks["on_x_seconds_after_step"] = None
-    
+
     def on_x_seconds_before_step(self):
         """Get On Seconds X Before Step Callback.
 
         :return: Callbacks
         """
         return self._callbacks["on_x_seconds_before_step"]
-    
+
     def add_on_x_seconds_before_step(self, callback, seconds: int):
         """
         Add callback function/method/object to execute before
@@ -68,7 +80,7 @@ class SmartSimulation(Simulation):
         """
         if self._is_callback(callback):
             self._callbacks["on_x_seconds_before_step"] = callback
-    
+
     def on_x_seconds_after_step(self):
         """Get On Seconds X After Step Callback.
 
@@ -93,6 +105,34 @@ class CoupledSimulation(SmartSimulation):
         super().__init__(**kwargs)
         self._coupled_model = coupled_model
         self._coupled_data = coupled_data
+        self.results = SimulationResults(self)
+
+    def __next__(self):
+        """Next"""
+        # Start Simulation
+        self.start()
+        # Check if simulation termination request was made
+        if self._terminate_request:
+            self._execute_callback(self.before_end())
+            raise StopIteration
+        # Execute Callback Hooks Before Simulation Step
+        self._execute_callback(self.before_step())
+        # Simulation Step Amount
+        if self._advance_seconds is None:
+            time = self._model.swmm_step()
+        else:
+            time = self._model.swmm_stride(self._advance_seconds)
+        # Execute Callback Hooks After Simulation Step
+        self._execute_callback(self.after_step())
+        self._execute_coupling_logic()
+        if time <= 0.0:
+            self._execute_callback(self.before_end())
+            raise StopIteration
+        return self._model
+
+    def _execute_coupling_logic(self):
+        ...
+
 
 # with Simulation(SWMM_path) as sim:
 #     s_list=[]
@@ -101,8 +141,8 @@ class CoupledSimulation(SmartSimulation):
 #     l_list=[]
 #     s_areas=[]
 #     for s in s_names_list: #lista de strings de los nombres de las subcuencas en .inp
-#         s_list.append(Subcatchments(sim)[s]) 
-#         s_areas.append(Subcatchments(sim)[s].area) #[ha]  
+#         s_list.append(Subcatchments(sim)[s])
+#         s_areas.append(Subcatchments(sim)[s].area) #[ha]
 #     for su in su_names_list)): #lista de strings de los nombres de las unidades de almacenamiento en .inp
 #         su_list.append(Nodes(sim)[su])
 #     for j in j_names_list: #lista de strings de los nombres de los nodos (junctions, outfalls and dividers) en .inp
