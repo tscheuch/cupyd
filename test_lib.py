3dc3c33ab0f3aa9314ba6eca14dc2c3d63c9012d
jricci1 <joaquin.ricci@thenotcompany.com>
2022-10-03 21:47:39 -0300

    Initial simulation logic

--- test_lib.py
+++ test_lib.py
@@ -1,8 +1,14 @@
 import platform
+import warnings
 from pathlib import Path
+from typing import List
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 import flopy
 import matplotlib.pyplot as plt
+import numpy
+from pyswmm import Links, Nodes, Simulation, Subcatchments
 
 from georef import CoupledModel
 from simulation.simulation import CoupledSimulation
@@ -31,9 +37,129 @@ coupled_model = CoupledModel(
     ml, subcatchment_shp, storage_units_shp_file_path, nodes_shp_file_path
 )
 
-with CoupledSimulation(coupled_model=coupled_model, coupled_data=None,  inputfile="llanquihue/SWMM/Llanquihue_base.inp") as sim:
+
+def test_before_start(self):
+    print("ON BEFORE START")
+    print(self)
+    print(self.current_time)
+    print("OUT BEFORE START")
+
+
+with CoupledSimulation(
+    coupled_model=coupled_model,
+    coupled_data=None,
+    inputfile="llanquihue/SWMM/Llanquihue_base.inp",
+) as sim:
+    # sim.add_before_step(test_before_start)
+    # sim.step_advance(86400) # 1 day
+    sim.step_advance(3600)  # 1 hour
+    hours = 0
+    subcatchments = [subcatchment for subcatchment in Subcatchments(sim)]
+    storage_units = [node for node in Nodes(sim) if node.is_storage()]
+    junctions = [node for node in Nodes(sim) if node.is_junction()]
+    conduits = [link for link in Links(sim) if link.is_conduit()]
+    subcatchments_cumulative_infiltration = {
+        subcatchment.subcatchmentid: 0 for subcatchment in subcatchments
+    }
+    storage_units_cumulative_infiltration = {
+        storage_unit.nodeid: 0 for storage_unit in storage_units
+    }
     for step in sim:
-        print(step.current_time)
+        hours += 1
+        # print(sim.current_time)
+        subcatchment_time_serie = sim.results.subcatchments_time_series_result
+        storage_units_time_serie = sim.results.storage_units_time_series_result
+        junctions_time_serie = sim.results.junctions_time_series_result
+        conduits_time_serie = sim.results.conduits_time_series_result
+        for subcatchment in subcatchments:
+            new_row = {
+                "Time": sim.current_time,
+                "Precipitation (mm/h)": subcatchment.rainfall,
+                "Evaporation (mm/d)": subcatchment.evaporation_loss,
+                "Infiltration (mm/h)": subcatchment.infiltration_loss,
+                "Runoff (m3/s)": subcatchment.runoff,
+                "Runon (m3/s)": subcatchment.runon,
+                "Cumulative Infiltration (m3)": subcatchment.statistics["infiltration"],
+                "Cumulative Evaporation (m3)": subcatchment.statistics["evaporation"],
+            }
+            subcatchment_time_serie[
+                subcatchment.subcatchmentid
+            ] = subcatchment_time_serie[subcatchment.subcatchmentid].append(
+                new_row, ignore_index=True
+            )
+
+        for storage_unit in storage_units:
+            new_row = {
+                "Time": sim.current_time,
+                "Depth (m)": storage_unit.depth,
+                "Head (m)": storage_unit.head,
+                "Flooding (m3/s)": storage_unit.flooding,
+                "Lateral inflow (m3/s)": storage_unit.lateral_inflow,
+                "Total inflow (m3/s)": storage_unit.total_inflow,
+                "Total outflow (m3/s)": storage_unit.total_outflow,
+                "Volume (m3)": storage_unit.volume,
+                "Losses (m3/s)": storage_unit.losses,
+                "Cumulative Exfiltration Loss (m3)": storage_unit.storage_statistics[
+                    "exfil_loss"
+                ],
+                "Cumulative Evaporation Loss (m3)": storage_unit.storage_statistics[
+                    "evap_loss"
+                ],
+            }
+            storage_units_time_serie[storage_unit.nodeid] = storage_units_time_serie[
+                storage_unit.nodeid
+            ].append(new_row, ignore_index=True)
+
+        for junction in junctions:
+            new_row = {
+                "Time": sim.current_time,
+                "Depth (m)": junction.depth,
+                "Head (m)": junction.head,
+                "Flooding (m3/s)": junction.flooding,
+                "Lateral inflow (m3/s)": junction.lateral_inflow,
+                "Total inflow (m3/s)": junction.total_inflow,
+                "Total outflow (m3/s)": junction.total_outflow,
+            }
+            junctions_time_serie[junction.nodeid] = junctions_time_serie[
+                junction.nodeid
+            ].append(new_row, ignore_index=True)
+
+        for conduit in conduits:
+            new_row = {
+                "Time": sim.current_time,
+                "Depth (m)": conduit.depth,
+                "Flow (m3/s)": conduit.flow,
+            }
+            conduits_time_serie[conduit.linkid] = conduits_time_serie[
+                conduit.linkid
+            ].append(new_row, ignore_index=True)
+
+        # print(subcatchment_time_serie._elements)
+        # s_SWMM_ts[i] =s_SWMM_ts[i].append(new_row, ignore_index=True)
+        if hours % 24 == 0:
+            print(sim.current_time)
+
+            modflow_recharge_from_subcatchment = {}
+            modflow_recharge_from_storage_units = {}
+            for subcatchment in subcatchments:
+                # Delta infiltration
+                modflow_recharge_from_subcatchment[subcatchment.subcatchmentid] = (
+                    subcatchment.statistics["infiltration"]
+                    - modflow_recharge_from_subcatchment[subcatchment.subcatchmentid]
+                )
+                subcatchments_cumulative_infiltration[
+                    subcatchment.subcatchmentid
+                ] = subcatchment.statistics["infiltration"]
+
+            for storage_unit in storage_units:
+                # Delta infiltration
+                modflow_recharge_from_storage_units[storage_unit.nodeid] = (
+                    storage_unit.statistics["exfil_loss"]
+                    - modflow_recharge_from_storage_units[storage_unit.nodeid]
+                )
+                storage_units_cumulative_infiltration[
+                    storage_unit.nodeid
+                ] = storage_unit.statistics["exfil_loss"]
 
 gdf_final = coupled_model.geo_dataframe
 
