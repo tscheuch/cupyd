433aeff20632a163e3f776553d5f439e235ae44b
jricci1 <joaquin.ricci@thenotcompany.com>
2022-11-03 17:07:56 -0300

    Make code run for the first time

--- test_lib.py
+++ test_lib.py
@@ -1,7 +1,9 @@
 import platform
 import warnings
 from pathlib import Path
+import os
 from typing import List
+import time
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
@@ -9,7 +11,7 @@ import flopy
 import matplotlib.pyplot as plt
 import numpy
 import pandas
-from pyswmm import Links, Nodes, Simulation, Subcatchments
+from pyswmm import Links, Nodes, Simulation, Subcatchments, SystemStats
 
 from georef import CoupledModel
 from simulation.simulation import CoupledSimulation
@@ -25,6 +27,9 @@ if platform.system() == "Darwin":
     exe_name = "mfnwt"
 
 
+exe_name = Path.joinpath(MODFLOW_WORKSPACE, exe_name)
+print(exe_name, MODFLOW_WORKSPACE)
+
 ml = flopy.modflow.Modflow.load(
     modelname, version="mfnwt", exe_name=exe_name, model_ws=MODFLOW_WORKSPACE
 )
@@ -38,6 +43,24 @@ coupled_model = CoupledModel(
     ml, subcatchment_shp, storage_units_shp_file_path, nodes_shp_file_path
 )
 
+# print(coupled_model.geo_dataframe["drn_cond"].describe())
+# print(len(coupled_model.geo_dataframe["drn_cond"]))
+# ml.drn.plot()
+# plt.show()
+# print(ml.bas6.ibound)
+# print(ml.bas6.ibound[0].string)
+# print(ml.bas6.ibound[0].array[0])
+# print(len(ml.bas6.ibound[0].array[0]))
+# print(coupled_model.geo_dataframe["ibound"])
+# print(coupled_model.geo_dataframe["ibound"].describe(include="all"))
+# coupled_model.geo_dataframe.plot(column="ibound")
+# plt.show()
+# ml.bas6.plot()
+# plt.show()
+# raise
+
+
+t1 = time.time()
 
 def test_before_start(self):
     print("ON BEFORE START")
@@ -51,6 +74,8 @@ with CoupledSimulation(
     coupled_data=None,
     inputfile="llanquihue/SWMM/Llanquihue_base.inp",
 ) as sim:
+
+    system_routing = SystemStats(sim)
 
     nrows = sim.modflow_model.dis.nrow
     ncols = sim.modflow_model.dis.ncol
@@ -77,6 +102,7 @@ with CoupledSimulation(
     storage_unit_area_dataframe = sim._coupled_model.geo_dataframe.groupby(
         "infiltration_storage_unit"
     ).sum()["area"]
+    print("STARTING SIMULATION")
     for step in sim:
         hours += 1
         # print(sim.current_time)
@@ -150,7 +176,7 @@ with CoupledSimulation(
         # print(subcatchment_time_serie._elements)
         # s_SWMM_ts[i] =s_SWMM_ts[i].append(new_row, ignore_index=True)
         if hours % 24 == 0:
-            print(sim.current_time)
+            print("TIME: ", sim.current_time)
 
             modflow_recharge_from_subcatchment = {}
             modflow_recharge_from_storage_units = {}
@@ -221,6 +247,10 @@ with CoupledSimulation(
             ) + dataframe_with_recharges.infiltration_storage_unit_recharge.fillna(
                 0
             )
+            if hours % 7200 == 0:
+                dataframe_with_recharges.plot(column="iteration_recharge")
+                print("PLOTING RECHARGE ")
+                plt.show()
 
             # Create MODFLOW inputs: RCH package
 
@@ -242,23 +272,176 @@ with CoupledSimulation(
                 sim.modflow_model, nrchop=3, rech=top_layer_recharge_matrix, ipakcb=53
             )
 
-            print(
-                dataframe_with_recharges[
-                    dataframe_with_recharges.subcatchment.notnull()
-                ]
-            )
-            print(
-                dataframe_with_recharges[
-                    dataframe_with_recharges.infiltration_storage_unit.notnull()
-                ]
-            )
-            print(
-                dataframe_with_recharges[
-                    dataframe_with_recharges.iteration_recharge.notnull()
-                ]
-            )
-            print(sim._coupled_model.geo_dataframe.iloc[0]["geometry"])
-            print(sim._coupled_model.geo_dataframe.iloc[0]["geometry"].area)
+            # Run MODFLOW
+            
+            # TODO: Improve performance by writing only necessary packages
+            sim.modflow_model.write_input()
+            sim.modflow_model.run_model(silent=True)
+            
+            #Read MODFLOW outputs
+            # headfile, _,  _ = sim.modflow_model.load_results()
+            fname = os.path.join(MODFLOW_WORKSPACE, 'LLANQUIHUE.hds')
+            headfile = flopy.utils.HeadFile(fname, model=sim.modflow_model)
+            heads = headfile.get_data()
+            heads[heads==1.e+30] = numpy.nan            # fix masked data 
+            heads[heads==-999.99] = numpy.nan
+
+            #Strt next loop
+            strt = heads[0]
+
+
+            ibound = numpy.ones((1,nrows,ncols)) 
+
+            for _, cell in dataframe_with_recharges.iterrows():
+                ibound[:,cell['x']-1,cell['y']-1] = cell['ibound']
+
+            bas = flopy.modflow.ModflowBas(sim.modflow_model, ibound=ibound, strt=strt) #use the head table of the last time step and bc
+
+            # print(
+            #     dataframe_with_recharges[
+            #         dataframe_with_recharges.subcatchment.notnull()
+            #     ]
+            # )
+            # print(
+            #     dataframe_with_recharges[
+            #         dataframe_with_recharges.infiltration_storage_unit.notnull()
+            #     ]
+            # )
+            # print(
+            #     dataframe_with_recharges[
+            #         dataframe_with_recharges.iteration_recharge.notnull()
+            #     ]
+            # )
+            # print(sim._coupled_model.geo_dataframe.iloc[0]["geometry"])
+            # print(sim._coupled_model.geo_dataframe.iloc[0]["geometry"].area)
+
+            # TODO: PREGUNTAR TERUCA
+            # Profundidad a la que drena una columna de Modflow (Solo  nos importa la top layer)
+            DRN_burn_depth=0.0
+            # Global parameters needed to calculate drain conductance (see reference MODELMUSE DRN package pane)
+            W = 5000     #model size (X)
+            H = 5000     #model size (Y)
+            x_resolution = W/ncols    
+            y_resolution = H/nrows
+            DRN_L = x_resolution 
+            DRN_W = y_resolution
+            DRN_M = 1
+            DRN_K = 0.05 #m/dia      
+
+            DRN_C=DRN_K*DRN_L*DRN_W/DRN_M
+
+            top = sim.modflow_model.dis.top.array
+            DTWT = (top - DRN_burn_depth) - heads[0]  
+
+            #DRN calculation
+            delta_H=numpy.reshape(DTWT, len(dataframe_with_recharges))
+            altura=numpy.reshape(heads[0], len(dataframe_with_recharges))
+            for i in range(len(delta_H)):
+                if delta_H[i]<0:
+                    delta_H[i]=-delta_H[i]
+                else:
+                     delta_H[i]=0.
+            dataframe_with_recharges["Altura"]=altura   
+            dataframe_with_recharges["delta_H"]=delta_H
+            dataframe_with_recharges["DRN_rate"]=0.
+            
+            # TODO: ASK TERE WHAT `DRN` IS
+            for i in range(len(dataframe_with_recharges)):
+                dataframe_with_recharges["DRN_rate"][i]=(delta_H[i])*dataframe_with_recharges["drn_cond"].fillna(0)[i]
+                # if dataframe_with_recharges["DRN"][i]==1:
+                    # dataframe_with_recharges["DRN_rate"][i]=(delta_H[i])*DRN_C
+                    # print(DRN_C, dataframe_with_recharges["drn_cond"][i])
+                # if ml.bas6.ibound[0].array[i]==-1:
+                # if dataframe_with_recharges["ibound"][i]==-1:
+                if dataframe_with_recharges["ibound"][i] == -1:
+                    dataframe_with_recharges["DRN_rate"][i]=0
+            
+
+            #INFLOW RATES IN SU AND JUNCTIONS
+            
+            #Inflow rates in SU:
+            
+            # node_inflow=dataframe_with_recharges.groupby("drn_to").sum()["DRN_rate"]
+            node_inflow = dataframe_with_recharges.groupby("node").sum()["DRN_rate"]
+            if hours % 7200 == 0:
+                t2 = time.time()
+                print(t2 - t1)
+                t1 = time.time()
+                print("PLOTING DRN RATE ")
+                dataframe_with_recharges.plot(column="DRN_rate")
+                plt.show()
+            # node_inflow_list = []
+            # print(node_inflow.index)
+            # storage_units = [node for node in Nodes(sim) if node.is_storage()]
+            # junctions = [node for node in Nodes(sim) if node.is_junction()]
+            for node in Nodes(sim):
+                inflow = node_inflow[node.nodeid]/86400. if node.nodeid in node_inflow.index else 0  # m3/s
+                node.generated_inflow(inflow)
+                # if node.nodeid in node_inflow.index:
+                #     node_inflow_list.append(node_inflow[node.nodeid])
+                # else:
+                #     node_inflow_list.append(0.)
+    
+                    
+            #Inflow rate in Junctions
+            
+            # junction_inflow=dataframe_with_recharges.groupby("drn_to").sum()["DRN_rate"]
+            # junction_inflow = dataframe_with_recharges.groupby("node").sum()["DRN_rate"]
+            # junction_inflow_list=[]
+            # for j in j_names_list:
+            #     if j in junction_inflow.index:
+            #         junction_inflow_list.append(junction_inflow[j])
+            #     else:
+            #         junction_inflow_list.append(0.)
+                
+            #Generated inflow SWMM WSU:
+            
+            # for i in range(len(su_list)):
+            #     rate=node_inflow_list[i]/86400. #m3/s
+            #     su_list[i].generated_inflow(rate)
+                
+                
+            #Generated inflow SWMM Junctions:
+            
+            # for i in range(len(j_list)):
+            #     rate=junction_inflow_list[i]/86400 #m3/s
+            #     j_list[i].generated_inflow(rate)
+                
+            #Save MODFLOW cells information
+    routing_stats=system_routing.routing_stats
+    runoff_stats=system_routing.runoff_stats
+                    
+print("Flow Routing Mass Balance Error:", sim.flow_routing_error)
+print("Runoff Mass Balance Error:", sim.runoff_error)
+
+df_routing_couple = pd.DataFrame({'Dry Weather Inflow (m3)': [routing_stats['dry_weather_inflow']],
+    'Wet Weather Inflow (m3)': [routing_stats['wet_weather_inflow']], 
+    'Ground Water Inflow (m3)': [routing_stats['groundwater_inflow']],
+    #'RDII Inflow (m3)': [routing_stats['II_inflow']],
+    'External Inflow (m3)': [routing_stats['external_inflow']],
+    'External Outflow (m3)': [routing_stats['outflow']],
+    'Flooding Loss (m3)': [routing_stats['flooding']],
+    'Evaporation Loss (m3)': [routing_stats['evaporation_loss']],
+    'Seepage Loss (m3)': [routing_stats['seepage_loss']],
+    'Initial Stored Volume': [routing_stats['initial_storage']],
+    'Final Stored Volume': [routing_stats['final_storage']]})
+
+df_runoff_couple = pd.DataFrame({'Total Precipitation (mm)': [runoff_stats['rainfall']],
+    'Evaporation Loss (m3)': [runoff_stats['evaporation']], 
+    'Infiltration Loss (m3)': [runoff_stats['infiltration']],
+    'Runoff (m3)': [runoff_stats['runoff']],
+    "Initial Storage (mm)":[runoff_stats['init_storage']],
+    "Final Storage (mm)":[runoff_stats['final_storage']]})
+            # TS_gdf.append(dataframe_with_recharges)
+            
+                
+            # #Save zonebudget DataFrame
+            # fname = os.path.join('LLANQUIHUE_MF_workspace', 'LLANQUIHUE.cbc')
+            # cbb = flopy.utils.CellBudgetFile(fname)
+            # zb = flopy.utils.ZoneBudget(cbb, zon, aliases=aliases)
+            # zb_df=zb.get_dataframes()
+            # ZB_TS.append(zb_df)
+            
 
 gdf_final = coupled_model.geo_dataframe
 
